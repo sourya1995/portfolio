39d7db476e06886d05dce77145bcfa1d8898b634
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-22 16:27:41 +0530

    removing socials

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -9,14 +9,6 @@ const Footer = () => {
                 <p className="footer-contact-access">Mobile: +91 97403 75575</p>
                 <p><a href="https://www.freepik.com/free-vector/cute-cat-working-laptop-cartoon-icon-illustration_13149230.htm#query=cat%20laptop&position=3&from_view=search&track=ais">Image by catalyststuff</a> on Freepik</p>
             </div>
-            <div>
-                <h1>Social Links</h1>
-                <div className="social-icons">
-                    <a href="https://www.educative.io/"><i><FaFacebook /></i></a>
-                    <a href="https://www.educative.io/"><i><FaInstagram /></i></a>
-                    <a href="https://www.educative.io/"><i><FaTwitter /></i></a>
-                </div>
-            </div>
         </footer>
     )
 }
